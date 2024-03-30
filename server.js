859f08ad419954ab50b83805de26934cec69f260
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-03-30 14:55:33 +0700

    commit

--- server.js
+++ server.js
@@ -94,5 +94,5 @@ app.get('/getasset', (req,res) => {
 
 const port = 3002;
 app.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });

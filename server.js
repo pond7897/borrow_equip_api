4891a294d51545ae330b4ea7b3d0d625322dc12a
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-05-16 22:52:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,140 @@
 const express = require("express");
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const db = require("./db");
 const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const passport = require("passport");
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+app.use(
+  expressSession({
+    secret: "mySecretKey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+//user
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig.js")(passport);
+app.use(cookieParser("mySecretKey"));
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+app.get("/", (req, res) => {  
+  res.send("Hello World!");
+  //   const query = "SELECT * FROM tbAsset";
+
+  //   db.query(query, (err, result) => {
+  //     if (err) {
+  //       throw err;
+  //     } else {
+  //       res.send(result);
+  //     }
+  //   });
+});
+app.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Database connected");
+app.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      res.send("No user found");
+    }
+    if (user) {
+      req.login(user, (err) => {
+        if (err) {
+          throw err;
+        }
+        res.send(req.user);
+        // console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+app.get("/getUser", (req, res) => {
+  if (!req.user) {
+    throw Error("Not logged in");
   }
-})
+  res.send(req.user);
+});
 
-app.get("/", (req, res) => {
-  const query = "SELECT * FROM tbAsset";
-
+app.get("/getalluser", (req, res) => {
+  const query = "SELECT * FROM tbStudent";
   db.query(query, (err, result) => {
     if (err) {
       throw err;
     } else {
       res.send(result);
     }
-  })
+  });
 });
 
-app.get("/getuser", (req,res) => {
-  const stuId = req.query.stuId
-  if (!stuId) {
-    res.status(400).send("ไม่พบเลขนักศึกษา")
-  }
-
-  const values = [stuId]
-
-  const query = "SELECT * FROM tbStudent WHERE fdStuId = ?";
-  db.query(query, values, (err, result) => {
-    if (err) {
-      throw err;
-    } else {
-      res.send(result);
+app.post("/addborrow", (req, res) => {
+  const values = {
+    fdStuId: req.user.fdStuId,
+    fdAssetId: req.body.fdAssetId,
+    fdBorrowDate: req.body.fdBorrowDate,
+    fdReturnDate: req.body.fdReturnDate,
+    fdReason: req.body.fdReason,
+  };
+  // console.log([values])
+  const query = `INSERT INTO tbBorrow (fdStuId, fdAssetId, fdBorrowDate, fdReturnDate, fdReason) VALUES (?, ?, ?, ?, ?)`;
+  db.query(
+    query,
+    [
+      values.fdStuId,
+      values.fdAssetId,
+      values.fdBorrowDate,
+      values.fdReturnDate,
+      values.fdReason,
+    ],
+    (err, result) => {
+      if (err) {
+        res.status(500).send({
+          error: "An error occurred while inserting data into tbBorrow.",
+        });
+      } else {
+        const query_editAsset = `UPDATE tbAsset SET fdStatus = 1, fdCount = fdCount + 1 WHERE fdAssetId = ?`;
+        db.query(query_editAsset, [values.fdAssetId], (err, result) => {
+          if (err) {
+            res.status(500).send({
+              error: "An error occurred while updating data in tbAsset.",
+            });
+          } else {
+            res
+              .status(200)
+              .send({ success: true, message: "Borrow added successfully." });
+          }
+        });
+      }
     }
-  })
-})
+  );
+});
 
-app.get("/getuser", (req,res) => {
-  const query = "SELECT * FROM tbStudent";
-  db.query(query, values, (err, result) => {
-    if (err) {
-      throw err;
-    } else {
-      res.send(result);
-      
-    }
-  })
-})
-
-app.post('/addborrow', (req, res) => {
-  const values = [req.body.stuId, req.body.assetId, req.body.borrowDate, req.body.returnDate]
-  const query = "INSERT INTO tbBorrow (fdStuId, fdAssetId, fdBorrowDate, fdReturnDate) VALUES ?";
+app.get("/getreturn", (req, res) => {
+  const values = req.user.fdStuId;
+  const query = `select b.fdId, b.fdStuId, s.fdFname, s.fdLname, b.fdAssetId, a.fdAssetName, b.fdBorrowDate, b.fdReturnDate, b.fdReason
+                  from tbBorrow b
+                  join tbStudent s on b.fdStuId = s.fdStuId
+                  join tbAsset a on b.fdAssetId = a.fdAssetId
+                  where b.fdStuId = ?`
   db.query(query, [values], (err, result) => {
     if (err) {
       throw err;
@@ -78,12 +142,37 @@ app.post('/addborrow', (req, res) => {
       res.send(result);
     }
   })
-}
-)
+});
 
-app.get('/getasset', (req,res) => {
-  const query = "SELECT * FROM tbAsset";
-  db.query(query, (err, result) => {
+app.post("/deleteborrow", (req, res) => {
+  const values = {
+    fdAssetId: req.body.fdAssetId
+  };
+  const query = `delete from tbBorrow where fdAssetId = ?;`
+  db.query(query, [values.fdAssetId], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      const query_editAsset = `UPDATE tbAsset SET fdStatus = 2 WHERE fdAssetId = ?`;
+      db.query(query_editAsset, [values.fdAssetId], (err, result) => {
+        if (err) {
+          res.status(500).send({
+            error: "An error occurred while updating data in tbAsset.",
+          });
+        } else {
+          res
+            .status(200)
+            .send({ success: true, message: "Borrow delete successfully." });
+        }
+      });
+    }
+  })
+});
+
+app.post("/editasset", (req, res) => {
+  const values = req.user.fdStuId;
+  const query = `delete from tbBorrow where fdStuId = ?;`
+  db.query(query, [values], (err, result) => {
     if (err) {
       throw err;
     } else {
@@ -92,7 +181,77 @@ app.get('/getasset', (req,res) => {
   })
 })
 
-const port = 3002;
+app.put("/updateAsset", (req, res) => {
+  const values = {
+    fdStatus:req.body.fdStatus,
+    fdAssetId: req.body.fdAssetId
+  };
+  const query = `UPDATE tbAsset SET fdStatus = ? WHERE fdAssetId = ?;`;
+  db.query(query, [values.fdStatus, values.fdAssetId], (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  })
+})
+
+app.post("/addhistory", (req, res) => {
+  const values = {
+    fdStuId: req.user.fdStuId,
+    fdAssetId: req.body.fdAssetId,
+    fdAssetName: req.body.fdAssetName,
+    fdBorrowDate: req.body.fdBorrowDate,
+    fdReturnDate: req.body.fdReturnDate,
+  };
+
+  const query_history = `INSERT INTO tbHistory (fdStuId, fdAssetId, fdAssetName, fdBorrowDate, fdReturnDate) VALUES (?, ?, ?, ?, ?)`;
+  db.query(
+    query_history,
+    [
+      values.fdStuId,
+      values.fdAssetId,
+      values.fdAssetName,
+      values.fdBorrowDate,
+      values.fdReturnDate,
+    ],
+    (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.get("/gethistory", (req, res) => {
+  const values = req.user;
+  const query = `select h.fdId,h.fdAssetId, h.fdStuId, h.fdBorrowDate, h.fdReturnDate, h.fdAssetName, a.fdFname 
+                  from tbHistory h left join tbAdmin a 
+                  on h.fdAdmin = a.fdEmail WHERE fdStuId = ?`;
+  // const query = "select * from tbHistory WHERE fdStuId = ?";
+  db.query(query, values.fdStuId, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/getAsset", (req, res) => {
+  const query = "SELECT * FROM tbAsset";
+  db.query(query, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+const port = 3001;
 app.listen(port, () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
